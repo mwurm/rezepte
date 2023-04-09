9057e73c9e6bb89a10c95ba8c06a9d771023177f
Matthias Wurm <mwurm@users.noreply.github.com>
2023-04-09 08:17:09 +0000

    category Reisgerichte

--- rmd_to_asciidoc.py
+++ rmd_to_asciidoc.py
@@ -63,7 +63,7 @@ class Cookbook:
 """)
 
         
-        cookbook_categories = ["Basis", "Appetithäppchen", "Beilagen", "Salate", "Suppen", "Pasta", "Pizza & Co.", "Aufläufe", "Fleischgerichte", "Geflügel", "Fisch", "Desserts", "Mehlspeisen", "Gebäck", "Kuchen"]
+        cookbook_categories = ["Basis", "Appetithäppchen", "Beilagen", "Salate", "Suppen", "Pasta", "Pizza & Co.", "Aufläufe", "Reisgerichte", "Fleischgerichte", "Geflügel", "Fisch", "Desserts", "Mehlspeisen", "Gebäck", "Kuchen"]
         for recipe in self.recipes:
             if recipe.category not in cookbook_categories:
                 raise Exception(f"Recipe {recipe.name} uses unknown category {recipe.category}")                
